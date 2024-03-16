d77d3cf4bd42986592cbc74fdeaeddf1ed251c35
Беркман Айдын <68331365+Melonber@users.noreply.github.com>
2024-03-16 21:27:06 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 // App.js
 import React, { useState } from 'react';
 import axios from 'axios';
+import CryptoList from './CryptoList';
 
 function App() {
   const [search, setSearch] = useState('');
@@ -27,12 +28,58 @@ function App() {
 
   return (
     <div>
-      <input
-        type="text"
-        value={search}
-        onChange={handleSearchChange}
-        placeholder="Enter Crypto Symbol (e.g., BTC)"
-      />
+       <div id="search">
+	<svg viewBox="0 0 420 60" xmlns="http://www.w3.org/2000/svg">
+		<rect class="bar"/>
+		
+		<g class="magnifier">
+			<circle class="glass"/>
+			<line class="handle" x1="32" y1="32" x2="44" y2="44"></line>
+		</g>
+
+		<g class="sparks">
+			<circle class="spark"/>
+			<circle class="spark"/>
+			<circle class="spark"/>
+		</g>
+
+		<g class="burst pattern-one">
+			<circle class="particle circle"/>
+			<path class="particle triangle"/>
+			<circle class="particle circle"/>
+			<path class="particle plus"/>
+			<rect class="particle rect"/>
+			<path class="particle triangle"/>
+		</g>
+		<g class="burst pattern-two">
+			<path class="particle plus"/>
+			<circle class="particle circle"/>
+			<path class="particle triangle"/>
+			<rect class="particle rect"/>
+			<circle class="particle circle"/>
+			<path class="particle plus"/>
+		</g>
+		<g class="burst pattern-three">
+			<circle class="particle circle"/>
+			<rect class="particle rect"/>
+			<path class="particle plus"/>
+			<path class="particle triangle"/>
+			<rect class="particle rect"/>
+			<path class="particle plus"/>
+		</g>
+	</svg>
+	<input 
+  type="search"
+  value={search}
+  onChange={handleSearchChange}
+  
+  placeholder="Enter Crypto Symbol (e.g., BTC)" name="q" aria-label="Search for inspiration"/>
+</div>
+
+<div id="results">
+	
+</div>
+
       <button onClick={handleSearch}>Search</button>
       {crypto && (
         <div>
@@ -42,8 +89,16 @@ function App() {
           <p>Current Price: ${crypto.current_price}</p>
         </div>
       )}
+      <div className="App">
+      <CryptoList />
     </div>
+    
+
+    </div>
+    
   );
+  
 }
+
 
 export default App;
